c6981b96cdd268f8cfcdb624cc010d16c0d46a8a
josef <gabrielsson@josef.st>
2020-09-25 10:49:20 +0200

    New simple structure for assigments

---
+++ data/assigments.go
@@ -0,0 +1,30 @@
+package data
+
+type Assigment struct {
+	ID          int
+	Methodology string
+	Roles       string
+	Description string
+	Project     string
+}
+
+func getAssigments() []*Assigment {
+	return assigmentList
+}
+
+var assigmentList = []*Assigment{
+	&Assigment{
+		ID:          1,
+		Methodology: "Software Development",
+		Roles:       "Front-end GIS Developer & DevOps",
+		Description: ".NET and React development of a plugin that can measure carbon dioxide emissions over different selected areas for Hajk (GIS solution based on OpenLayers). Pipelining a CI/CD with Azure DevOps using YAML to deploy the application.",
+		Project:     "Karta",
+	},
+	&Assigment{
+		ID:          2,
+		Methodology: "Software Development",
+		Roles:       "Full-stack Developer & DevOps",
+		Description: ".NET development, UI/UX design, and implementation of a webpage with MVC called Skolmuseet, listing old artifacts items, links, and articles. Pipelining a CI/CD with Azure DevOps using YAML to deploy the application.",
+		Project:     "Skolmuseet",
+	},
+}
