69a6081da274c4adab10e72e6f21363f20066105
josef <gabrielsson@josef.st>
2020-09-25 03:45:15 +0200

    Handler in a package

--- main.go
+++ main.go
@@ -1,31 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+
+	"./handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(rw, "Something went wrong", http.StatusInternalServerError)
-			return
-		}
 
-		fmt.Fprintf(rw, "Hello World! %s", d)
-	})
+	// References
+	l := log.New(os.Stdout, "api", log.LstdFlags)
+	hh := handlers.NewHello(l)
 
-	http.HandleFunc("/post", func(rw http.ResponseWriter, r *http.Request) {
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(rw, "Something went wrong", http.StatusBadRequest)
-			return
-		}
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
 
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
-
-	http.ListenAndServe(":888", nil)
+	http.ListenAndServe(":888", sm)
 }
