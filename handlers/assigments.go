ca3cfede047800d5d71a18a2bc9ec28abdf73640
josef <gabrielsson@josef.st>
2020-09-25 15:02:47 +0200

    Support more methods

--- handlers/assigments.go
+++ handlers/assigments.go
@@ -15,7 +15,17 @@ func NewAssigments(l *log.Logger) *Assigments {
 	return &Assigments{l}
 }
 
-func (a *Assigments) ServeHTTP(rw http.ResponseWriter, h *http.Request) {
+func (a *Assigments) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	switch method := r.Method; method {
+	case http.MethodGet:
+		a.GetAssigments(rw, r)
+		return
+	}
+
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (a *Assigments) GetAssigments(rw http.ResponseWriter, r *http.Request) {
 	a.l.Println("Hello Assigments!")
 
 	la := data.GetAssigments()
