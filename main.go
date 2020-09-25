a0c8c4e13f6bb938535bee1681ebb69779d188a2
josef <gabrielsson@josef.st>
2020-09-25 10:23:52 +0200

    Graceful shutdown

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 
 	"./handlers"
@@ -26,5 +28,21 @@ func main() {
 		WriteTimeout: 1 * time.Second,
 	}
 
-	s.ListenAndServe()
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sc := make(chan os.Signal)
+	signal.Notify(sc, os.Interrupt)
+	signal.Notify(sc, os.Kill)
+
+	sig := <-sc
+	l.Println("Recieved terminate, graceful shutdown", sig)
+
+	tc, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	s.Shutdown(tc)
 }
